f42d4f4bfea7cc6ecc60b12d360ab32b96a59ed8
Livia <livia.cassiano.ifpr.2023@gmail.com>
2025-08-27 15:05:28 -0300

    Estruturas condicionais2
    Formação da página Ex00

---
+++ Wstruturas_Condicionais/js/ex00.js
@@ -0,0 +1,11 @@
+var nome, idade
+
+nome=prompt("Digite seu nome:")
+idade=parseInt(prompt("Digite sua idade:"))
+
+if(idade>=18){
+    alert(nome+" você pode tentar tirar a CNH")
+}
+else{
+    alert(nome+ " você não está apto para tirar a CNH")
+}
