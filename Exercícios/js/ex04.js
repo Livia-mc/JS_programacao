716d11d0642cc96d9f1eb0f49aa32d491f3b25f9
Livia-mc <livia.cassiano.ifpr.2023@gmail.com>
2025-09-10 14:08:06 -0300

    Ex 4 pronto

---
+++ Exercícios/js/ex04.js
@@ -0,0 +1,14 @@
+var n1, n2, n3, n4
+n1=parseInt(prompt("Digite sua primeira nota: "))
+n2=parseInt(prompt("Digite sua segunda nota: "))
+n3=parseInt(prompt("Digite sua terceira nota: "))
+n4=parseInt(prompt("Digite sua quarta nota: "))
+
+alert("Sua média é: " + (n1+n2+n3+n4)/4)
+
+if ((n1+n2+n3+n4)/4 >= 7) {
+    alert("Você foi aprovado")
+}
+else {
+    alert("Você foi reprovado")
+}
