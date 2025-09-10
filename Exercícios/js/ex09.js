16a47bbe365e223bb3c27a46d2dde415e3063c51
Livia-mc <livia.cassiano.ifpr.2023@gmail.com>
2025-09-10 14:34:30 -0300

    Ex 9 pronto

---
+++ Exercícios/js/ex09.js
@@ -0,0 +1,12 @@
+var n1
+n1=parseInt(prompt("Digite o número: "))
+
+if (n1>=1) {
+    alert("Seu número é possitivo")
+}
+else {
+    if (n1<0) {
+        alert("Seu número é negativo")
+    }
+    alert("Seu número é nulo")
+}
