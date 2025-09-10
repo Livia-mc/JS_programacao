6fac1a1c35ae217b0060459eb62daa28be70b088
Livia-mc <livia.cassiano.ifpr.2023@gmail.com>
2025-09-10 14:27:25 -0300

    pronto 8

--- Exercícios/js/ex07.js
+++ Exercícios/js/ex07.js
@@ -2,8 +2,6 @@ var n1, n2
 n1=parseInt(prompt("Digite o primeiro número: "))
 n2=parseInt(prompt("Digite o segundo número: "))
 
-alert("Sua média é: " + (n1+n2+n3+n4)/4)
-
 if (n1+n2>=10) {
     alert("A soma é "+(n1+n2))
 }
---
+++ Exercícios/js/ex08.js
@@ -0,0 +1,10 @@
+var n1, n2
+n1=parseInt(prompt("Digite o primeiro número: "))
+n2=parseInt(prompt("Digite o segundo número: "))
+
+if (n1+n2>20) {
+    alert("O resultado é "+(n1+n2+8))
+}
+else {
+    alert("NO resulatado é "+(n1+n2-5))
+}
