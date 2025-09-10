0554281e016aafd1abefd7fd85fd5ffaf15596dd
Livia-mc <livia.cassiano.ifpr.2023@gmail.com>
2025-09-10 14:23:39 -0300

    O 7

---
+++ Exercícios/js/ex07.js
@@ -0,0 +1,12 @@
+var n1, n2
+n1=parseInt(prompt("Digite o primeiro número: "))
+n2=parseInt(prompt("Digite o segundo número: "))
+
+alert("Sua média é: " + (n1+n2+n3+n4)/4)
+
+if (n1+n2>=10) {
+    alert("A soma é "+(n1+n2))
+}
+else {
+    alert("Não é possivel mostrar seu resultado")
+}
